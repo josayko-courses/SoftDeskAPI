e035d76a05752d978639e7411e93ba78d8530709
Jonny Saykosy <josayko@pm.me>
2022-03-23 12:20:56 +0100

    update error handlers 2

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,5 @@
-from django.urls import include, path
+from django.urls import path
 
-from rest_framework import routers
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from api.views import (
