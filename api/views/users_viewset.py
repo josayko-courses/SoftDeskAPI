bc511cc49a05c67824268493289e03bf5fedebf6
Jonny Saykosy <josayko@pm.me>
2022-03-30 11:17:27 +0200

    update UsersViewset.create()

--- api/views/users_viewset.py
+++ api/views/users_viewset.py
@@ -18,7 +18,12 @@ class UsersViewset(ModelViewSet):
     def create(self, request, *args, **kwargs):
         serializer = UserCreateSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.create_user(**request.data)
+            serializer.create_user(
+                email=request.data["email"],
+                password=request.data["password"],
+                first_name=request.data["first_name"],
+                last_name=request.data["last_name"],
+            )
             return Response(
                 {"email": serializer.data["email"]},
                 status.HTTP_201_CREATED,
